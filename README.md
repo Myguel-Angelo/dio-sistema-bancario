3e5dd90960745e518494b7f72906862d3f24bbcd
Angelo <angelo.nunes576@gmail.com>
2024-06-04 13:55:11 -0300

    docs: Parte 2 da documentação

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Projeto de Estudos DIO - Sistema Bancário
 
-Neste projeto, você terá a oportunidade de criar um Sistema Bancário em Python. O objetivo é implementar três operações essenciais: depósito, saque e extrato. O sistema será desenvolvido para um banco que busca monetizar suas operações. Durante o desafio, você terá a chance de aplicar seus conhecimentos em programação Python e criar um sistema funcional que simule as operações bancárias. Prepare-se para aprimorar suas habilidades e demonstrar sua capacidade de desenvolver soluções práticas e eficientes.
+### Parte 1
+Neste projeto, tive a oportunidade de criar um Sistema Bancário em Python. O objetivo é implementar três operações essenciais: depósito, saque e extrato. O sistema será desenvolvido para um banco que busca monetizar suas operações. Durante o desafio, você terá a chance de aplicar seus conhecimentos em programação Python e criar um sistema funcional que simule as operações bancárias.
+
+### Parte 2
+Neste desafio, tive a oportunidade de otimizar o Sistema Bancário previamente desenvolvido com o uso de funções Python. O objetivo é aprimorar a estrutura e a eficiência do sistema, implementando as operações de depósito, saque e extrato em funções específicas. tive a chance de refatorar o código existente, dividindo-o em funções reutilizáveis, facilitando a manutenção e o entendimento do sistema como um todo.
+
+**OBS**: Na Parte 1, Eu já havia colocado funções
--- projeto-bancario.py
+++ projeto-bancario.py
@@ -1,48 +1,77 @@
-# Inicializa a lista de extratos e o saldo da conta
+lista_cpfs = []
+lista_usuários = []
+lista_contas = []
+
+numero_conta = 0
 lista_extratos = []
-saldo = 1000.00
+saldo_padrao = 0
 
-# Função para salvar o extrato de operações
-def salvar_extrato(operacao, valor, saldo):
-    global lista_extrato
-    
-    # Adiciona um extrato formatado à lista de extratos
-    lista_extratos.append(f'''        
-    Operação de {operacao} no valor de R$ {valor:.2f}
-    Saldo final: {saldo:.2f}
-    ''')
 
-# Função para depositar um valor na conta
-def depositar(valor):
-    global saldo
-    print('O valor foi depositado a sua conta')
+# FUNÇÕES DA ÁREA DE LOGIN:
+def cadastrar_usuario():
+    print('Cadastrando novo usuário...')
+    global lista_cpfs, lista_usuários
+    nome = input('Nome: ')
+    cpf = input('CPF: ')
     
-    # Salva o extrato:
-    saldo += valor
-    salvar_extrato('Depósito', valor, saldo)
+    if cpf in lista_cpfs:
+        print('O Cpf já esta cadastrado em um usuário')
+        return 'Erro'
+    
+    lista_cpfs.append(cpf)
+    data = input('Data de Nascimento: ')
+    endereco = input('Endereço: ')
+    lista_usuários.append(
+        {cpf: {'Nome': nome, 'Data Nascimento': data, 'Endereço': endereco}}
+    )
 
-# Função para sacar um valor da conta
-def sacar(valor, saques_realizados):
-    global saldo
-    if saques_realizados == 3:            # Verifica se o limite de saques diários foi atingido
-        print('   Limite de Saque diário estourado!\nPor favor tente novamente amanhã...')
-        return
-    if valor > saldo:                     # Verifica se o valor do saque é maior que o saldo disponível
-        print(f'   Operação não Realizada\nValor disponível para saque: R$ {saldo:.2f}')
-        return
-    if valor > 500:                       # Verifica se o valor do saque é maior que o limite permitido
-        print('   Valor máximo de saque: R$500,00\nPor favor tente novamente...')
-        return
+def criar_conta_corrente():
+    print('Criando nova Conta Corrente...')
+    global lista_cpfs, numero_conta, lista_usuários
+    cpf_digitado = input('Informe o CPF do Usuário: ')
     
-    print('Sacando Valor! retire abaixo do caixa')
+    if cpf_digitado not in lista_cpfs:
+        print('Usuário não identificado, informe um cpf válido!')
+        return 'Erro'
     
-    # Salva o extrato: 
-    saldo -= valor
-    salvar_extrato('Saque', valor, saldo)
-    return True
+    numero_conta+=1
+    agencia_numero = f'0001-{numero_conta}'
+    for user in lista_usuários:
+        if cpf_digitado in tuple(user.keys()):
+            dono_conta = user[cpf_digitado]['Nome'] 
+        
+    lista_contas.append(
+        {agencia_numero: cpf_digitado}
+    )
+    print(f'Agencia e Numero da conta: {agencia_numero}, Nome do dono: {dono_conta}')
 
-# Interface do usuário
-interface = '''
+
+def entrar():
+    global lista_contas
+    print('Digite a Agência e o Número da Conta')
+    entrada = input('>>> ')
+    for conta in lista_contas:
+        if entrada in tuple(conta.keys()):
+            return conta
+    print('Conta Corrente não Encontrada')
+    return None
+
+
+# Interfaces do banco
+interface_login = '''
+---------------------------------------------------------
+                BANCO DIO - ÁREA DE LOGIN
+---------------------------------------------------------
+
+Ações disponíveis:
+1. Cadastrar Usuário
+2. Criar Conta-Corrente
+3. Entrar
+4. Encerrar
+
+obs: para criar uma conta, é necessário um usuário'''
+
+interface_user = '''
 ---------------------------------------------------------
                BANCO DIO - ÁREA DE USUÁRIO
 ---------------------------------------------------------
@@ -55,48 +84,131 @@ Opções de Operação:
 
 obs: Saques de até R$ 500,00
 ---------------------------------------------------------'''
-print(interface)
 
-saques_realizados = 0 # Contador de saques realizados
-
-while True:
-    print(f'\nSaldo atual: R$ {saldo:.2f}')
-    operacao = input('Operação: ')
+def abrir_area_usuario(conta):
+    global saldo_padrao, lista_extratos
+    lista_extratos = []
+    saldo_padrao = 1000.00
     
-    if operacao == '1':
-        valor_deposito = input('Digite o valor do depósito: ').replace(',', '.')
-        
-        # Tenta converter o valor do depósito para float e realizar a operação
-        try:
-            depositar(float(valor_deposito))
-        except ValueError:
-            print('Valor digitado inválido!')
 
-    elif operacao == '2':
-        valor_saque = input('Digite o valor do saque: ').replace(',', '.')
+    # FUNÇÕES DA ÁREA DE USUÁRIO:
+    def salvar_extrato(operacao, valor, saldo):
+        global lista_extrato
         
-        # Tenta converter o valor do saque para float e realizar a operação
-        try:
-            if sacar(float(valor_saque), saques_realizados):
-                saques_realizados += 1
-        except ValueError:
-            print('Valor digitado inválido!')
+        # Adiciona um extrato formatado à lista de extratos
+        lista_extratos.append(f'''        
+        Operação de {operacao} no valor de R$ {valor:.2f}
+        Saldo final: {saldo:.2f}
+        ''')
+
+    def depositar(valor):
+        global saldo_padrao
+        print('O valor foi depositado a sua conta')
         
-    elif operacao == '3':
-        # Verifica se a lista de extratos está vazia
-        if len(lista_extratos) == 0:
-            print('Lista de Extratos Vazia! ')
-            continue
+        # Salva o extrato:
+        saldo_padrao += valor
+        salvar_extrato('Depósito', valor, saldo_padrao)
+
+    def sacar(valor, saques_realizados):
+        global saldo_padrao
+        if saques_realizados == 3:            # Verifica se o limite de saques diários foi atingido
+            print('   Limite de Saque diário estourado!\nPor favor tente novamente amanhã...')
+            return
+        if valor > saldo_padrao:              # Verifica se o valor do saque é maior que o saldo disponível
+            print(f'   Operação não Realizada\nValor disponível para saque: R$ {saldo_padrao:.2f}')
+            return
+        if valor > 500:                       # Verifica se o valor do saque é maior que o limite permitido
+            print('   Valor máximo de saque: R$500,00\nPor favor tente novamente...')
+            return
         
-        # Exibe todos os extratos salvos
-        print('Extratos da conta:')
-        for extrato in lista_extratos:
-            print(extrato)
+        print('Sacando Valor! retire abaixo do caixa')
+        
+        # Salva o extrato: 
+        saldo_padrao -= valor
+        salvar_extrato('Saque', valor, saldo_padrao)
+        return True
+
+    
+    global lista_contas, lista_usuários, interface_login
+    print(
+        'Entrando na conta localizada:',
+        f'Dados: {conta}'
+    )
+    print(interface_user)
+    saques_realizados = 0 # Contador de saques realizados
+
+    while True:
+        print(f'\nSaldo atual: R$ {saldo_padrao:.2f}')
+        operacao = input('Operação: ')
+        
+        if operacao == '1':
+            valor_deposito = input('Digite o valor do depósito: ').replace(',', '.')
             
-    elif operacao == '4':
-        # Finaliza o programa
-        print('Obrigado por utilizar o Banco DIO, até mais!')
+            # Tenta converter o valor do depósito para float e realizar a operação
+            try:
+                depositar(float(valor_deposito))
+            except ValueError:
+                print('Valor digitado inválido!')
+
+        elif operacao == '2':
+            valor_saque = input('Digite o valor do saque: ').replace(',', '.')
+            
+            # Tenta converter o valor do saque para float e realizar a operação
+            try:
+                if sacar(float(valor_saque), saques_realizados):
+                    saques_realizados += 1
+            except ValueError:
+                print('Valor digitado inválido!')
+            
+        elif operacao == '3':
+            # Verifica se a lista de extratos está vazia
+            if len(lista_extratos) == 0:
+                print('Lista de Extratos Vazia! ')
+                continue
+            
+            # Exibe todos os extratos salvos
+            print('Extratos da conta:')
+            for extrato in lista_extratos:
+                print(extrato)
+                
+        elif operacao == '4':
+            # Finaliza o programa
+            print('Obrigado por utilizar o Banco DIO, até mais!')
+            print('\n', interface_login)
+            break
+        
+        else:
+            print('Digite uma operação existente, por favor!')
+
+
+print(interface_login)
+while True:
+    acao = input('\nAção: ')
+    if acao == '1':
+        if cadastrar_usuario() is not None:
+            print('Tente cadastrar novamente...')
+            continue
+        print('Usuário Cadastrado com SUCESSO!')
+        
+    elif acao == '2':
+        if len(lista_usuários) == 0:
+            print('Ainda não há usuários Cadastrados\nNão será possível criar uma conta corrente')
+            continue
+        if criar_conta_corrente() is not None:
+            print('Tente criar a conta novamente...')
+            continue
+        print('Conta Corrente criada com SUCESSO!')
+        
+    elif acao == '3':
+        conta_logada = entrar()
+        if conta_logada is None:
+            print('Tente em uma conta existemte, ou crie uma nova')
+            continue
+        abrir_area_usuario(conta_logada)
+        
+    elif acao == '4':
+        print('ENCERRANDO O SISTEMA...')
         break
     
     else:
-        print('Digite uma operação existente, por favor!')
+        print('Digite uma ação existente, por favor!')
